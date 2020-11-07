ec7590a09bbc3bef0b029fee01a2b56d775e5d8b
suvadityamuk <suvadityamuk@gmail.com>
2020-11-07 10:53:36 +0530

    Initial commit

---
+++ app/src/main/java/com/example/practiceapp/MainActivity.java
@@ -0,0 +1,87 @@
+package com.example.practiceapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.net.Uri;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+
+import static android.widget.Toast.*;
+
+public class MainActivity extends AppCompatActivity {
+    //public TextView souptext;
+
+    public MainActivity() throws IOException {
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+    public void add(View v)
+    {
+            EditText a = findViewById(R.id.op1);
+            EditText b = findViewById(R.id.op2);
+            double p = Double.parseDouble(a.getText().toString());
+            double q = Double.parseDouble(b.getText().toString());
+            double sum = p + q;
+            ((EditText)findViewById(R.id.res)).setText(String.valueOf(sum));
+
+    }
+    public void sub(View v)
+    {
+        EditText a = findViewById(R.id.op1);
+        EditText b = findViewById(R.id.op2);
+        double p = Double.parseDouble(a.getText().toString());
+        double q = Double.parseDouble(b.getText().toString());
+        double diff = p - q;
+        ((EditText)findViewById(R.id.res)).setText(String.valueOf(diff));
+
+    }
+    public void mul(View v)
+    {
+        EditText a = findViewById(R.id.op1);
+        EditText b = findViewById(R.id.op2);
+        double p = Double.parseDouble(a.getText().toString());
+        double q = Double.parseDouble(b.getText().toString());
+        double prod = p * q;
+        ((EditText)findViewById(R.id.res)).setText(String.valueOf(prod));
+
+    }
+    public void div(View v)
+    {
+        EditText a = findViewById(R.id.op1);
+        EditText b = findViewById(R.id.op2);
+        double p = Double.parseDouble(a.getText().toString());
+        double q = Double.parseDouble(b.getText().toString());
+        double quot = p / q;
+        ((EditText)findViewById(R.id.res)).setText(String.valueOf(quot));
+    }
+    /*public void activationjsoup(View v) throws IOException {
+        TextView souptext = findViewById(R.id.textforsoup);
+        Button b = (Button) findViewById(R.id.activate);
+        URL myurl = new URL("https://jsoup.org/apidocs/org/jsoup/nodes/class-use/Document.html");
+        Document content = Jsoup.parse(myurl, 5000);
+        //Document res = Jsoup.parse(String.valueOf(content), "UTF-8");
+        String words = content.text();
+        souptext.setText(words);
+
+
+    }*/
+}
+
